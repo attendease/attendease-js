042bc0d9a71601b0169c9fa6121ec80e301b72c9
Michael Richards <mike22e@gmail.com>
2015-01-15 13:54:17 -0800

    Refactor the Client#instances function to use ES5 array methods.

--- src/client/instances.js
+++ src/client/instances.js
@@ -2,18 +2,14 @@
 exports.instances = function(sync) {
   var def = $.Deferred()
   var instances = []
-  var i, ii, session, instance
 
   this.sessions().then(function(sessions) {
-    for (i = 0; i < sessions.length; i++) {
-      session = sessions[i]
-
-      for (ii = 0; ii < (session.instances || []).length; ii++) {
-        instance = session.instances[ii]
+    sessions.forEach(function(session) {
+      (session.instances || []).forEach(function(instance) {
         instance.session = session
         instances.push(instance)
-      }
-    }
+      })
+    })
 
     def.resolve(instances)
   }, function() {
