61fed58232f83fec318e473534d05b4e6ee9ab82
Michael Richards <mike22e@gmail.com>
2015-06-16 12:11:25 -0700

    Build 0.5.0.

--- dist/attendease.js
+++ dist/attendease.js
@@ -1,41 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -84,6 +84,7 @@
 	util.extend(Client.prototype, __webpack_require__(7))
 	util.extend(Client.prototype, __webpack_require__(8))
 	util.extend(Client.prototype, __webpack_require__(9))
+	util.extend(Client.prototype, __webpack_require__(10))
 
 	// Export
 	module.exports = Client
@@ -432,5 +433,55 @@
 	}
 
 
+/***/ },
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// Checks in the current user.
+	exports.checkin = function(type, extra) {
+	  var data = this.credentials()
+
+	  switch (type) {
+	    case 'event':
+	      data.type = 'Event'
+	      break
+	    case 'instance':
+	      data.type = 'Instance'
+	      data.instance_id = extra
+	      break
+	    case 'beacon':
+	      data.type = 'Beacon'
+	      data.beacon = extra
+	  }
+
+	  return $.ajax({
+	    type: "POST",
+	    url: this.apiRoot() + 'api/checkin',
+	    data: data
+	  })
+	}
+
+	// Undos a check-in for the current user.
+	exports.undoCheckin = function(type, extra) {
+	  var data = this.credentials()
+
+	  switch (type) {
+	    case 'event':
+	      data.type = 'Event'
+	      break
+	    case 'instance':
+	      data.type = 'Instance'
+	      data.instance_id = extra
+	      break
+	  }
+
+	  return $.ajax({
+	    type: "POST",
+	    url: this.apiRoot() + 'api/checkin_undo',
+	    data: data
+	  })
+	}
+
+
 /***/ }
-/******/ ])
+/******/ ]);
